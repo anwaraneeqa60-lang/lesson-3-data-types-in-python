539caa7be298f27646b9f87075ca4c6fac1af528
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-01-02 20:46:12 -0500

    type casting.py

---
+++ type casting.py
@@ -0,0 +1,19 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("name :", name)
+print("data type of name is", type(name))
+print("age :", age)
+print("data types of age is", type(age))
+print("is_student :", is_student)
+print("data type of is_student", type (is_student))
+print("weight:", weight)
+print("data types of is_student is", type(weight))
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("data types of age is ",type(age))
+weight = int(weight)
+print(weight)
+print("data types of weight is", type(weight))
